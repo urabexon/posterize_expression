32eb960af37c3fb3aa83dfd54d503f6e6adc9aa5
urabexon <hiroki708.a48uk@gmail.com>
2025-01-22 20:08:42 +0900

    fix all

--- myscript.js
+++ myscript.js
@@ -1,16 +1,14 @@
 window.addEventListener('load', () => {
-	const container = document.getElementById('container');
-	const normalImg = document.getElementById('normal');
-	const posterized = document.getElementById('posterized');
-	const paragraph = document.getElementById('paragraph');
-
-	document.body.style.height = `${window.innerHeight * 2}px`;
-	container.style.height = `${window.innerHeight}px`;
-	paragraph.style.marginLeft = `${normalImg.clientWidth}px`;
-
+	const container = document.getElementById('container')
+	const normalImg = document.getElementById('normal')
+	const posterized = document.getElementById('posterized')
+	const paragraph = document.getElementById('paragraph')
+	document.body.style.height = `${window.innerHeight * 2}px`
+	container.style.height = `${window.innerHeight}px`
+	paragraph.style.marginLeft = `${normalImg.clientWidth}px`
 	window.addEventListener('scroll', (e) => {
-		const { scrollY } = window;
+	  	const { scrollY } = window
 		const ratio = 1 - (scrollY / window.innerHeight);
-		posterized.style.opacity = ratio;
-	});
-});
+		posterized.style.opacity = ratio
+	})
+})
